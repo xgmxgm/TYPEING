fcf2947530ffff0b4cf083e77f81970366bd7e0d
xgmxgm <109265751+xgmxgm@users.noreply.github.com>
2023-06-07 19:50:18 +0600

    TYPEING
    TYPEING

--- src/app/Layouts/ui/Layout/index.tsx
+++ src/app/Layouts/ui/Layout/index.tsx
@@ -2,12 +2,10 @@ import { FC, type PropsWithChildren } from 'react'
 
 import "./Layout.scss"
 import { Header } from '@/widgets/Header/ui/Header'
-import { Scroll } from '@/shared/ui/Scroll'
 
 export const Layout: FC<PropsWithChildren> = ({children}) => {
     return (
         <>
-            {/* <Scroll /> */}
             <Header />
             {children}
         </>
